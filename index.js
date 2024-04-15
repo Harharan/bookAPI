ed89dd89083abe9fbe826e8f3a0bbc1baf5d0d3b
Hariharan <k.n.hariharan@gmail.com>
2024-04-15 12:28:52 -0500

    Comment added in the remote deployment

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
+// const express = require("express");
+// const router = require('./router')
+// const mongoose = require("mongoose");
+
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+
+// dotenv.config();
+
+
+// const PORT = process.env.PORT || 3000;
+
+
+// const app = express();
+
+// mongoose
+//   .connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// app.use(cors());
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use(router);
+
+// app.listen(PORT, async () => {
+//   console.log(`server up on port ${PORT}`);
+// });
+  
 const express = require("express");
 const router = require('./router')
-const mongoose = require("mongoose");
-
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-dotenv.config();
-
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 
-const app = express();
-
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.use(cors());
-
+dotenv.config();
+ 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -35,4 +56,5 @@ app.use(router);
 app.listen(PORT, async () => {
   console.log(`server up on port ${PORT}`);
 });
-  
+
+
