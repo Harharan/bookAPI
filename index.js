3371e47c51a0b0a115edcfa187390766eb7b6280
Hariharan <k.n.hariharan@gmail.com>
2024-04-15 12:49:25 -0500

    Update the Port Number 3000

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// const express = require("express");
-// const router = require('./router')
-// const mongoose = require("mongoose");
+const express = require("express");
+const router = require('./router')
+const mongoose = require("mongoose");
 
-// const dotenv = require("dotenv");
-// const cors = require("cors");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-// dotenv.config();
-
-
-// const PORT = process.env.PORT || 3000;
+dotenv.config();
 
 
-// const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
 
 // mongoose
 //   .connect(process.env.MONGODB_URL, {
@@ -25,17 +25,19 @@
 //     console.log(err);
 //   });
 
-// app.use(cors());
+app.use(cors());
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// app.use(router);
+app.use(router);
 
-// app.listen(PORT, async () => {
-//   console.log(`server up on port ${PORT}`);
-// });
-  
+app.listen(PORT, async () => {
+  console.log(`server up on port ${PORT}`);
+});
+
+
+/*
 const express = require("express");
 const router = require('./router')
 const dotenv = require("dotenv");
@@ -58,3 +60,4 @@ app.listen(PORT, async () => {
 });
 
 
+*/
